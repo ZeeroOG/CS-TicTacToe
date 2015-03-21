b2010a3304b677645be0da1fc241010ba125ec6b
ZeeroOG <zeero@192.168.3.2>
2015-03-21 18:28:22 +0100

    Initial commit

---
+++ src/MainFrame.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace TicTacToe
+{
+    partial class MainFrame
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrame));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 69);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(92, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 69);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(170, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(72, 69);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(14, 117);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(72, 69);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(92, 117);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(72, 69);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(170, 117);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(72, 69);
+            this.button6.TabIndex = 3;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(14, 192);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(72, 69);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(92, 192);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(72, 69);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(170, 192);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(72, 69);
+            this.button9.TabIndex = 6;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Tic Tac Toe Game";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 264);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Created by ZeeroOG";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(106, 279);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel1.TabIndex = 11;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "GitHub";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // MainFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 301);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainFrame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ src/MainFrame.cs
@@ -0,0 +1,570 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class MainFrame : Form
+    {
+        public MainFrame()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.button1.Text == "")
+            {
+                this.button1.Text = "X";
+                nextMove();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.button2.Text == "")
+            {
+                this.button2.Text = "X";
+                nextMove();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (this.button3.Text == "")
+            {
+                this.button3.Text = "X";
+                nextMove();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (this.button4.Text == "")
+            {
+                this.button4.Text = "X";
+                nextMove();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (this.button5.Text == "")
+            {
+                this.button5.Text = "X";
+                nextMove();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (this.button6.Text == "")
+            {
+                this.button6.Text = "X";
+                nextMove();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (this.button7.Text == "")
+            {
+                this.button7.Text = "X";
+                nextMove();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (this.button8.Text == "")
+            {
+                this.button8.Text = "X";
+                nextMove();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (this.button9.Text == "")
+            {
+                this.button9.Text = "X";
+                nextMove();
+            }
+        }
+
+        private void nextMove()
+        {
+            if (IsGameFinished() == true)
+            {
+                return;
+            }
+            
+            int[] table = new int[10];
+            int[] level = new int[10];
+            level[0] = -2;
+            int somme = 0;
+
+            if (this.button1.Text == "O") { table[1] = 10; }
+            else if (this.button1.Text == "X") { table[1] = 1; }
+            else { table[1] = 0; }
+
+            if (this.button2.Text == "O") { table[2] = 10; }
+            else if (this.button2.Text == "X") { table[2] = 1; }
+            else { table[2] = 0; }
+
+            if (this.button3.Text == "O") { table[3] = 10; }
+            else if (this.button3.Text == "X") { table[3] = 1; }
+            else { table[3] = 0; }
+
+            if (this.button4.Text == "O") { table[4] = 10; }
+            else if (this.button4.Text == "X") { table[4] = 1; }
+            else { table[4] = 0; }
+
+            if (this.button5.Text == "O") { table[5] = 10; }
+            else if (this.button5.Text == "X") { table[5] = 1; }
+            else { table[5] = 0; }
+
+            if (this.button6.Text == "O") { table[6] = 10; }
+            else if (this.button6.Text == "X") { table[6] = 1; }
+            else { table[6] = 0; }
+
+            if (this.button7.Text == "O") { table[7] = 10; }
+            else if (this.button7.Text == "X") { table[7] = 1; }
+            else { table[7] = 0; }
+
+            if (this.button8.Text == "O") { table[8] = 10; }
+            else if (this.button8.Text == "X") { table[8] = 1; }
+            else { table[8] = 0; }
+
+            if (this.button9.Text == "O") { table[9] = 10; }
+            else if (this.button9.Text == "X") { table[9] = 1; }
+            else { table[9] = 0; }
+
+            somme = table[1] + table[2] + table[3];
+            if (somme == 10)
+            {
+                level[1]++;
+                level[2]++;
+                level[3]++;
+            }
+            else if (somme == 20)
+            {
+                level[1]++;
+                level[1]++;
+                level[2]++;
+                level[2]++;
+                level[3]++;
+                level[3]++;
+            }
+            else if (somme == 2)
+            {
+                level[1] += 100;
+                level[2] += 100;
+                level[3] += 100;
+            }
+
+            somme = table[3] + table[6] + table[9];
+            if (somme == 10)
+            {
+                level[3]++;
+                level[6]++;
+                level[9]++;
+            }
+            else if (somme == 20)
+            {
+                level[3]++;
+                level[3]++;
+                level[6]++;
+                level[6]++;
+                level[9]++;
+                level[9]++;
+            }
+            else if (somme == 2)
+            {
+                level[3] += 100;
+                level[6] += 100;
+                level[9] += 100;
+            }
+
+            somme = table[7] + table[8] + table[9];
+            if (somme == 10)
+            {
+                level[7]++;
+                level[8]++;
+                level[9]++;
+            }
+            else if (somme == 20)
+            {
+                level[7]++;
+                level[7]++;
+                level[8]++;
+                level[8]++;
+                level[9]++;
+                level[9]++;
+            }
+            else if (somme == 2)
+            {
+                level[7] += 100;
+                level[8] += 100;
+                level[9] += 100;
+            }
+
+            somme = table[1] + table[4] + table[7];
+            if (somme == 10)
+            {
+                level[1]++;
+                level[4]++;
+                level[7]++;
+            }
+            else if (somme == 20)
+            {
+                level[1]++;
+                level[1]++;
+                level[4]++;
+                level[4]++;
+                level[7]++;
+                level[7]++;
+            }
+            else if (somme == 2)
+            {
+                level[1] += 100;
+                level[4] += 100;
+                level[7] += 100;
+            }
+
+            somme = table[2] + table[5] + table[8];
+            if (somme == 10)
+            {
+                level[2]++;
+                level[5]++;
+                level[8]++;
+            }
+            else if (somme == 20)
+            {
+                level[2]++;
+                level[2]++;
+                level[5]++;
+                level[5]++;
+                level[8]++;
+                level[8]++;
+            }
+            else if (somme == 2)
+            {
+                level[2] += 100;
+                level[5] += 100;
+                level[8] += 100;
+            }
+
+            somme = table[4] + table[5] + table[6];
+            if (somme == 10)
+            {
+                level[4]++;
+                level[5]++;
+                level[6]++;
+            }
+            else if (somme == 20)
+            {
+                level[4]++;
+                level[4]++;
+                level[5]++;
+                level[5]++;
+                level[6]++;
+                level[6]++;
+            }
+            else if (somme == 2)
+            {
+                level[4] += 100;
+                level[5] += 100;
+                level[6] += 100;
+            }
+
+            somme = table[1] + table[5] + table[9];
+            if (somme == 10)
+            {
+                level[1]++;
+                level[5]++;
+                level[9]++;
+            }
+            else if (somme == 20)
+            {
+                level[1]++;
+                level[1]++;
+                level[5]++;
+                level[5]++;
+                level[9]++;
+                level[9]++;
+            }
+            else if (somme == 2)
+            {
+                level[1] += 100;
+                level[5] += 100;
+                level[9] += 100;
+            }
+
+            somme = table[3] + table[5] + table[7];
+            if (somme == 10)
+            {
+                level[3]++;
+                level[5]++;
+                level[7]++;
+            }
+            else if (somme == 20)
+            {
+                level[3]++;
+                level[3]++;
+                level[5]++;
+                level[5]++;
+                level[7]++;
+                level[7]++;
+            }
+            else if (somme == 2)
+            {
+                level[3] += 100;
+                level[5] += 100;
+                level[7] += 100;
+            }
+
+            if (this.button1.Text != "") { level[1] = -1; }
+            if (this.button2.Text != "") { level[2] = -1; }
+            if (this.button3.Text != "") { level[3] = -1; }
+            if (this.button4.Text != "") { level[4] = -1; }
+            if (this.button5.Text != "") { level[5] = -1; }
+            if (this.button6.Text != "") { level[6] = -1; }
+            if (this.button7.Text != "") { level[7] = -1; }
+            if (this.button8.Text != "") { level[8] = -1; }
+            if (this.button9.Text != "") { level[9] = -1; }
+
+            if (level.ToList().IndexOf(level.Max()) == 1)
+            {
+                this.button1.Text = "O";
+            }
+
+            if (level.ToList().IndexOf(level.Max()) == 2)
+            {
+                this.button2.Text = "O";
+            }
+
+            if (level.ToList().IndexOf(level.Max()) == 3)
+            {
+                this.button3.Text = "O";
+            }
+
+            if (level.ToList().IndexOf(level.Max()) == 4)
+            {
+                this.button4.Text = "O";
+            }
+
+            if (level.ToList().IndexOf(level.Max()) == 5)
+            {
+                this.button5.Text = "O";
+            }
+
+            if (level.ToList().IndexOf(level.Max()) == 6)
+            {
+                this.button6.Text = "O";
+            }
+
+            if (level.ToList().IndexOf(level.Max()) == 7)
+            {
+                this.button7.Text = "O";
+            }
+
+            if (level.ToList().IndexOf(level.Max()) == 8)
+            {
+                this.button8.Text = "O";
+            }
+
+            if (level.ToList().IndexOf(level.Max()) == 9)
+            {
+                this.button9.Text = "O";
+            }
+
+            IsGameFinished();
+        }
+
+        private bool IsGameFinished()
+        {
+            int[] table = new int[10];
+            int somme = 0;
+
+            if (this.button1.Text == "O") { table[1] = 10; }
+            else if (this.button1.Text == "X") { table[1] = 1; }
+            else { table[1] = 0; }
+
+            if (this.button2.Text == "O") { table[2] = 10; }
+            else if (this.button2.Text == "X") { table[2] = 1; }
+            else { table[2] = 0; }
+
+            if (this.button3.Text == "O") { table[3] = 10; }
+            else if (this.button3.Text == "X") { table[3] = 1; }
+            else { table[3] = 0; }
+
+            if (this.button4.Text == "O") { table[4] = 10; }
+            else if (this.button4.Text == "X") { table[4] = 1; }
+            else { table[4] = 0; }
+
+            if (this.button5.Text == "O") { table[5] = 10; }
+            else if (this.button5.Text == "X") { table[5] = 1; }
+            else { table[5] = 0; }
+
+            if (this.button6.Text == "O") { table[6] = 10; }
+            else if (this.button6.Text == "X") { table[6] = 1; }
+            else { table[6] = 0; }
+
+            if (this.button7.Text == "O") { table[7] = 10; }
+            else if (this.button7.Text == "X") { table[7] = 1; }
+            else { table[7] = 0; }
+
+            if (this.button8.Text == "O") { table[8] = 10; }
+            else if (this.button8.Text == "X") { table[8] = 1; }
+            else { table[8] = 0; }
+
+            if (this.button9.Text == "O") { table[9] = 10; }
+            else if (this.button9.Text == "X") { table[9] = 1; }
+            else { table[9] = 0; }
+
+            somme = table[1] + table[2] + table[3];
+            if (somme == 3)
+            {
+                WhoWon(2);
+                return true;
+            }
+            else if (somme == 30)
+            {
+                WhoWon(1);
+                return true;
+            }
+
+            somme = table[3] + table[6] + table[9];
+            if (somme == 3)
+            {
+                WhoWon(2);
+                return true;
+            }
+            else if (somme == 30)
+            {
+                WhoWon(1);
+                return true;
+            }
+
+            somme = table[7] + table[8] + table[9];
+            if (somme == 3)
+            {
+                WhoWon(2);
+                return true;
+            }
+            else if (somme == 30)
+            {
+                WhoWon(1);
+                return true;
+            }
+
+            somme = table[1] + table[4] + table[7];
+            if (somme == 3)
+            {
+                WhoWon(2);
+                return true;
+            }
+            else if (somme == 30)
+            {
+                WhoWon(1);
+                return true;
+            }
+
+            somme = table[2] + table[5] + table[8];
+            if (somme == 3)
+            {
+                WhoWon(2);
+                return true;
+            }
+            else if (somme == 30)
+            {
+                WhoWon(1);
+                return true;
+            }
+
+            somme = table[4] + table[5] + table[6];
+            if (somme == 3)
+            {
+                WhoWon(2);
+                return true;
+            }
+            else if (somme == 30)
+            {
+                WhoWon(1);
+                return true;
+            }
+
+            somme = table[1] + table[5] + table[9];
+            if (somme == 3)
+            {
+                WhoWon(2);
+                return true;
+            }
+            else if (somme == 30)
+            {
+                WhoWon(1);
+                return true;
+            }
+
+            somme = table[3] + table[5] + table[7];
+            if (somme == 3)
+            {
+                WhoWon(2);
+                return true;
+            }
+            else if (somme == 30)
+            {
+                WhoWon(1);
+                return true;
+            }
+
+            if (this.button1.Text != "" && this.button2.Text != "" && this.button3.Text != "" && this.button4.Text != "" && this.button5.Text != "" && this.button6.Text != "" && this.button7.Text != "" && this.button8.Text != "" && this.button9.Text != "")
+            {
+                WhoWon(0);
+                return true;
+            }
+
+            return false;
+        }
+
+        private void WhoWon(int WhoWon)
+        {
+            String Message = "";
+            if (WhoWon == 0) { Message = "Draw !"; }
+            else if (WhoWon == 1) { Message = "You lost !"; }
+            else if (WhoWon == 2) { Message = "You win !"; }
+            var reponse = MessageBox.Show(Message + "\nPlay again ?", "Game over !", MessageBoxButtons.YesNo, MessageBoxIcon.Hand);
+
+            if (reponse == DialogResult.No)
+            {
+                System.Windows.Forms.Application.Exit();
+            }
+            else
+            {
+                this.button1.Text = "";
+                this.button2.Text = "";
+                this.button3.Text = "";
+                this.button4.Text = "";
+                this.button5.Text = "";
+                this.button6.Text = "";
+                this.button7.Text = "";
+                this.button8.Text = "";
+                this.button9.Text = "";
+
+                this.button1.Focus();
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/ZeeroOG/");
+        }
+    }
+}
